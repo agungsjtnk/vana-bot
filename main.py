7cc9e0af58da00f81fe84e73b6287732c52d5d69
agungsjtnk <112896887+agungsjtnk@users.noreply.github.com>
2024-10-01 09:35:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,111 +1,42 @@
+from utils.core.telegram import Accounts
+from utils.starter import start, stats
+import asyncio
+from data import config
 import os
-import requests
-import time
 
-class Vana:
 
-    def headers(self, initData):
-        return {
-            "Accept": "*/*",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-            "X-Telegram-Web-App-Init-Data": initData
-        }
+async def main():
+    print("Soft's author: https://t.me/ApeCryptor\n")
+    action = 1
 
-    def log(self, msg):
-        print(f"[*] {msg}")
+    if not os.path.exists('sessions'): os.mkdir('sessions')
 
-    def wait_with_countdown(self, seconds):
-        for i in range(seconds, -1, -1):
-            print(f"[*] Mohon tunggu {i} detik untuk akun selanjutnya...", end='\r')
-            time.sleep(1)
-        print()
+    if config.PROXY['USE_PROXY_FROM_FILE']:
+        if not os.path.exists(config.PROXY['PROXY_PATH']):
+            with open(config.PROXY['PROXY_PATH'], 'w') as f:
+                f.write("")
+    else:
+        if not os.path.exists('sessions/accounts.json'):
+            with open("sessions/accounts.json", 'w') as f:
+                f.write("[]")
 
-    def get_player_data(self, initData):
-        url = 'https://www.vanadatahero.com/api/player'
-        headers = self.headers(initData)
-        try:
-            response = requests.get(url, headers=headers)
-            response.raise_for_status()  # Periksa status kode HTTP
-            return response.json()
-        except requests.exceptions.RequestException as e:
-            self.log('Terjadi kesalahan saat memanggil API')
-            print(e)
-            return None  # Mengembalikan None jika ada kesalahan
+    if action == 3:
+        await Accounts().create_sessions()
 
-    def post_task_completion(self, initData, taskId, points):
-        url = f'https://www.vanadatahero.com/api/tasks/{taskId}'
-        headers = self.headers(initData)
-        payload = {
-            "status": "completed",
-            "points": float(points)
-        }
-        try:
-            response = requests.post(url, json=payload, headers=headers)
-            response.raise_for_status()  # Periksa status kode HTTP
-            if response.json().get('message') == 'Points limit exceeded':
-                self.log('Berhasil melakukan semua tugas untuk hari ini!')
-                return False
-            return True
-        except requests.exceptions.RequestException as e:
-            self.log('Terjadi kesalahan saat menyelesaikan tugas')
-            print(e)
-            return False
+    if action == 2:
+        await stats()
 
-    def get_tasks(self, initData):
-        url = 'https://www.vanadatahero.com/api/tasks'
-        headers = self.headers(initData)
-        try:
-            response = requests.get(url, headers=headers)
-            response.raise_for_status()  # Periksa status kode HTTP
-            return response.json().get('tasks', [])
-        except requests.exceptions.RequestException as e:
-            self.log('Terjadi kesalahan saat mengambil daftar tugas')
-            print(e)
-            return []  # Mengembalikan list kosong jika ada kesalahan
+    if action == 1:
+        accounts = await Accounts().get_accounts()
 
-    def complete_pending_tasks(self, initData):
-        tasks = self.get_tasks(initData)
-        exclude_ids = [2, 17, 5, 9]
-        for task in tasks:
-            if not task.get('completed') and task['id'] not in exclude_ids:
-                success = self.post_task_completion(initData, task['id'], task['points'])
-                if success:
-                    self.log(f"Menjalankan tugas {task['name']} berhasil mendapatkan: {task['points']}")
+        tasks = []
 
-    def process_account(self, initData, accountIndex):
-        try:
-            playerData = self.get_player_data(initData)
-            if playerData:
-                print(f"========== Menjalankan akun {accountIndex} | {playerData['tgFirstName']} ==========")
-                self.log(f"Points: {playerData['points']}")
-                self.log(f"Multiplier: {playerData['multiplier']}")
-                
-                while True:
-                    points = round(40000.0 + (50000.0 - 40000.0) * time.time() % 1, 1)  # Menghitung poin acak
-                    taskCompleted = self.post_task_completion(initData, 1, points)
-                    if not taskCompleted:
-                        break
-                    updatedPlayerData = self.get_player_data(initData)
-                    if updatedPlayerData:
-                        self.log(f"Auto tap berhasil. Total Balance: {updatedPlayerData['points']}")
-                    time.sleep(1)
-                
-                self.complete_pending_tasks(initData)
-        except Exception as e:
-            self.log('Terjadi kesalahan saat memproses akun')
-            print(e)
+        for thread, account in enumerate(accounts):
+            session_name, phone_number, proxy = account.values()
+            tasks.append(asyncio.create_task(start(session_name=session_name, phone_number=phone_number, thread=thread, proxy=proxy)))
 
-    def main(self):
-        data_file = os.path.join(os.getcwd(), 'data.txt')
-        with open(data_file, 'r') as f:
-            initDataList = [line.strip() for line in f.readlines() if line.strip()]
-        for i, initData in enumerate(initDataList):
-            self.process_account(initData, i + 1)
-            self.wait_with_countdown(3)
-        self.wait_with_countdown(86400)  # Tunggu 24 jam
+        await asyncio.gather(*tasks)
 
-if __name__ == "__main__":
-    vana = Vana()
-    vana.main()
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
